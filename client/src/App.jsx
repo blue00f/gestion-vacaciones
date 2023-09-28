7b72028a829a657ffe8fa3128557e171dfc60d90
Bruno <brunoleonelroca@gmail.com>
2023-09-28 18:51:38 -0300

    Agregar react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom"
 import React from "react"
 import Header from "./components/Header"
 import Login from "./components/Login"
@@ -6,12 +7,18 @@ import Calendar from "./components/Calendar"
 import Dashboard from "./components/Dashboard.jsx"
 
 import "./index.css"
+import "flowbite-datepicker"
 
 function App() {
   return (
     <>
       <Header />
-      <Calendar />
+      <Routes>
+        <Route exact path="/" element={<Login />} />
+        <Route exact path="/calendar" element={<Calendar />} />
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/formemployee" element={<FormEmployee />} />
+      </Routes>
     </>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-
+import { Link } from "react-router-dom"
 function Header() {
   return (
     <header>
@@ -16,16 +16,16 @@ function Header() {
             </span>
           </a>
           <div className="flex items-center lg:order-2">
-            <a
-              href="#"
+            <Link
+              to="/"
               className="text-white focus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800">
               Log in
-            </a>
-            <a
-              href="#"
+            </Link>
+            <Link
+              to="/"
               className="text-white focus:ring-4 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800">
               Register
-            </a>
+            </Link>
             <button
               data-collapse-toggle="mobile-menu-2"
               type="button"
@@ -61,25 +61,25 @@ function Header() {
             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li></li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="dashboard"
                   className="block py-2 pr-4 pl-3 border-b lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700">
                   Dashboard
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="formemployee"
                   className="block py-2 pr-4 pl-3 border-b lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700">
                   Empleados
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
+                <Link
+                  to="calendar"
                   className="block py-2 pr-4 pl-3 border-b lg:border-0 lg:hover:text-primary-700 lg:p-0 text-gray-400 lg:hover:text-white hover:bg-gray-700 hover:text-white lg:hover:bg-transparent border-gray-700">
                   Calendarios
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
