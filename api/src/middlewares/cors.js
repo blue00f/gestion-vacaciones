8eb2f5e533ea6820f1883030ee9a0051ce8f565b
Bruno <brunoleonelroca@gmail.com>
2023-10-26 01:45:07 -0300

    Agregar CORS

--- api/src/middlewares/cors.js
+++ api/src/middlewares/cors.js
@@ -1,6 +1,10 @@
 import cors from 'cors'
 
-const ACCEPTED_ORIGINS = ['http://localhost:1234', 'http://localhost:5173']
+const ACCEPTED_ORIGINS = [
+  'http://localhost:1234',
+  'http://localhost:5173',
+  'https://gestion-vacaciones.vercel.app/',
+]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
   cors({
